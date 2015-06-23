b4a0b1c6a942b03eadfa19803dea030794d0cc08
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-06-23 02:50:16 -0400

    what now?

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@
 
 $(document).ready(function(){
 
-  $(".dropdown-link").mouseenter(function(){
+  /* $(".dropdown-link").mouseenter(function(){
     $(".dropdown-menu").slideDown(500);
   });
 
-  $(".dropdown-menu, .dropdown-link").mouseleave(function(){
+  $(".dropdown-menu").mouseleave(function(){
     $(".dropdown-menu").slideUp(500);
   });
 
   $('.dropdown-menu').mouseenter(function() {
         $(".dropdown-menu").show();
+    }); */
+
+
+  $('dropdown-link').hover(function() {
+        $('.dropdown-menu').slideDown(500);
+    }, function() {
+        $('.dropdown-menu').slideDown(500);
+    });
+
+    $('.dropdown-menu').mouseenter(function() {
+        $(this).show();
+    });
+
+    $('.dropdown-menu').mouseleave(function() {
+        $(this).hide();
     });
 
 });
