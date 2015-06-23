748522e9b7f01f1965c2d94ecd42d8b01e65ddf3
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2015-06-23 00:54:48 -0400

    niggaaa

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+/*--- Dropdown Menu Functionality ---*/
+
+$(document).ready(function() {
+  $('.heaver-nav > li').bind('mouseover', openSubMenu);
+  $('.header-nav > li').bind('mouseout', closeSubMenu);
+
+  function openSubMenu() {
+    $(this).find('.dropdown-menu > li').css('visibility', 'visible');
+  };
+  
+  function closeSubMenu() {
+    $(this).find('.dropdown-menu > li').css('visibility', 'hidden');
+  };
+
+
+});
